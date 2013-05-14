03b7404801dce86c2f625d28340798b3caf1991e
Nicola <nicola.licheri@gmail.com>
2013-05-14 19:14:56 +0200

    aggiunta metodi

--- slabs.cpp
+++ slabs.cpp
@@ -13,6 +13,8 @@ slab_allocator::slab_allocator
     this->mem_malloced = 0;
     this->mem_avail = 0;
 
+    this->slab_bulk_check = DEFAULT_SLAB_BULK_CHECK;
+
 /* slabs init */
 
     int i = POWER_SMALLEST - 1; /// SETTINGS
@@ -73,3 +75,142 @@ slab_allocator::slab_allocator
 }
 
 
+unsigned int slab_allocator::slabs_clsid(const size_t size) {
+    int res = POWER_SMALLEST;
+
+    if (size == 0)
+        return 0;
+
+    while (size > slabclass[res].size)
+        if (res++ == power_largest)     /* won't fit in the biggest slab */
+            return 0;
+
+    return res;
+}
+
+
+void* slab_allocator::slabs_alloc(size_t size, unsigned int id) {
+	return NULL;
+}
+
+
+void slab_allocator::slabs_free(void *ptr, size_t size, unsigned int id) {
+	;
+}
+
+
+enum reassign_result_type slab_allocator::slabs_reassign(int src, int dst) {
+	return (enum reassign_result_type) 0;
+}
+
+
+int slab_allocator::start_slab_maintenance_thread(void) {
+    int ret;
+    char *env = getenv("MEMCACHED_SLAB_BULK_CHECK");
+
+    this->slab_rebalance_signal = 0;
+    this->slab_rebal.slab_start = NULL;
+
+    if (env != NULL) {
+        this->slab_bulk_check = atoi(env);
+        if (this->slab_bulk_check == 0) {
+            this->slab_bulk_check = DEFAULT_SLAB_BULK_CHECK;
+        }
+    }
+
+    if (pthread_cond_init(&this->slab_rebalance_cond, NULL) != 0) {
+        std::cerr << "Can't initialize rebalance condition" << std::endl;
+        return -1;
+    }
+
+    pthread_mutex_init(&this->slabs_rebalance_lock, NULL);
+
+    if ((ret = pthread_create(
+            &this->maintenance_tid,
+            NULL,
+            (void* (*)(void*)) &slab_allocator::slab_maintenance_thread,
+            (void*) this)) != 0) {
+        std::cerr << "Can't reate slab maintenance thread: " << strerror(ret) << std::cerr;
+        return -1;
+    }
+    if ((ret = pthread_create(
+            &this->rebalance_tid,
+            NULL,
+            (void* (*)(void*)) &slab_allocator::slab_rebalance_thread,
+            (void*) this)) != 0) {
+        std::cerr << "Can't create rebalance thread: " << strerror(ret) << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
+
+
+void slab_allocator::stop_slab_maintenance_thread(void) {
+	;
+}
+
+
+void slab_allocator::slabs_rebalancer_pause(void) {
+	pthread_mutex_lock(&this->slabs_rebalance_lock);
+}
+
+
+void slab_allocator::slabs_rebalancer_resume(void) {
+	pthread_mutex_unlock(&this->slabs_rebalance_lock);
+}
+
+void* slab_allocator::slab_maintenance_thread(void *arg) {
+   int src, dest;
+   slab_allocator *s = (slab_allocator *) arg;
+
+    while (s->do_run_slab_thread) {
+        if (settings.slab_automove == 1) {
+            if (s->slab_automove_decision(&src, &dest) == 1) {
+                /* Blind to the return codes. It will retry on its own */
+                s->slabs_reassign(src, dest);
+            }
+            sleep(1);
+        } else {
+            /* Don't wake as often if we're not enabled.
+             * This is lazier than setting up a condition right now. */
+            sleep(5);
+        }
+    }
+
+    return NULL;
+}
+
+void* slab_allocator::slab_rebalance_thread(void *arg) {
+    int was_busy = 0;
+    slab_allocator *s = (slab_allocator*) arg;
+    /* So we first pass into cond_wait with the mutex held */
+    mutex_lock(&s->slabs_rebalance_lock);
+
+    while (s->do_run_slab_rebalance_thread) {
+        if (s->slab_rebalance_signal == 1) {
+            if (s->slab_rebalance_start() < 0) {
+                /* Handle errors with more specifity as required. */
+                s->slab_rebalance_signal = 0;
+            }
+
+            was_busy = 0;
+        } else if (s->slab_rebalance_signal && s->slab_rebal.slab_start != NULL) {
+            was_busy = s->slab_rebalance_move();
+        }
+
+        if (s->slab_rebal.done) {
+            s->slab_rebalance_finish();
+        } else if (was_busy) {
+            /* Stuck waiting for some items to unlock, so slow down a bit
+             * to give them a chance to free up */
+            usleep(50);
+        }
+
+        if (s->slab_rebalance_signal == 0) {
+            /* always hold this lock while we're running */
+            pthread_cond_wait(&s->slab_rebalance_cond, &s->slabs_rebalance_lock);
+        }
+    }
+    return NULL;
+}
--- slabs.hpp
+++ slabs.hpp
@@ -5,9 +5,10 @@
 #include "defines.h"
 
 #define MAX_NUMBER_OF_SLAB_CLASSES 1
+#define DEFAULT_SLAB_BULK_CHECK 1
+
 #define POWER_LARGEST 1
 #define POWER_SMALLEST 1
-
 
 #define CHUNK_ALIGN_BYTES 1
 
@@ -28,12 +29,37 @@ typedef struct {
     size_t requested; /* The number of requested bytes */
 } slabclass_t;
 
+struct slab_rebalance {
+    void *slab_start;
+    void *slab_end;
+    void *slab_pos;
+    int s_clsid;
+    int d_clsid;
+    int busy_items;
+    uint8_t done;
+};
+
+enum reassign_result_type {
+    REASSIGN_OK=0, REASSIGN_RUNNING, REASSIGN_BADCLASS, REASSIGN_NOSPARE,
+    REASSIGN_SRC_DST_SAME
+};
+
 
 class slab_allocator {
     private:
 /* Access to the slab allocator is protected by this lock */
         pthread_mutex_t slabs_lock;
         pthread_mutex_t slabs_rebalance_lock;
+
+        pthread_t rebalance_tid;
+        pthread_t maintenance_tid;
+
+        pthread_cond_t slab_rebalance_cond;
+
+        struct slab_rebalance slab_rebal;
+        int slab_rebalance_signal;
+
+        int slab_bulk_check;
 
         slabclass_t slabclass[MAX_NUMBER_OF_SLAB_CLASSES];
         size_t mem_limit;
@@ -52,4 +78,31 @@ class slab_allocator {
     up front (if true), or allocate memory in chunks as it is needed (if false)
 */
         slab_allocator(const size_t limit, const double factor = 1.25, const bool prealloc = false);
+
+/** Given object size, return id to use when allocating/freeing memory for object
+ *  0 means error: can't store such a large object */
+        unsigned int slabs_clsid(const size_t size);
+
+/** Allocate object of given length, 0 on error */
+        void *slabs_alloc(size_t size, unsigned int id);
+
+/** Free previously allocated object */
+        void slabs_free(void *ptr, size_t size, unsigned int id);
+
+        enum reassign_result_type slabs_reassign(int src, int dst);
+
+        int start_slab_maintenance_thread(void);
+        void stop_slab_maintenance_thread(void);
+
+        void slabs_rebalancer_pause(void);
+        void slabs_rebalancer_resume(void);
+
+/** Slab rebalancer thread.
+ * Does not use spinlocks since it is not timing sensitive. Burn less CPU and
+ * go to sleep if locks are contended */
+        void *slab_maintenance_thread(void *arg);
+
+/** Slab mover thread.
+ * Sits waiting for a condition to jump off and shovel some memory about */
+        void *slab_rebalance_thread(void *arg);
 };
