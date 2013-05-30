9794d76d2a39eb084049900dc7ff428406bcba61
Nicola <nicola.licheri@gmail.com>
2013-05-30 12:22:54 +0200

    incapsulamento funzioni mutex

---
+++ mutex.cpp
@@ -0,0 +1,28 @@
+#include "mutex.h"
+
+
+mutex::mutex() {
+    pthread_mutex_init(&this->l, NULL);
+}
+
+int mutex::lock() {
+    return pthread_mutex_lock(&this->l);
+}
+
+int mutex::lock_trylock() {
+    while (this->mutex_trylock())   ;
+    return 0;
+}
+
+int mutex::trylock() {
+    return pthread_mutex_trylock(&this->l);
+}
+
+int mutex::unlock() {
+    return pthread_mutex_unlock(&this->l);
+}
+
+int mutex::cond_wait(pthread_cond_t *cond) {
+    return pthread_cond_wait(cond, &this->l);
+}
+
