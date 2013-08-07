ca3d73be250cdef0437eba43ac204978582d01ef
Nicola <nicola.licheri@gmail.com>
2013-08-08 01:15:24 +0200

    sorgenti di testing

--- src/zmqcache.cpp
+++ src/zmqcache.cpp
@@ -1,5 +1,6 @@
 #include <zmq.hpp>
 #include <iostream>
+#include <unistd.h>
 
 #include "datacache.hpp"
 //#include "threadpool.hpp"
@@ -16,6 +17,7 @@ void *server(void *arg);
 
 
 int main() {
+    pid_t pid = getpid();
     datathread_t data;
     zmq::context_t context(1);
 
@@ -27,7 +29,9 @@ int main() {
     zmq::socket_t workers (context, ZMQ_DEALER);
     workers.bind("inproc://workers");
 
-    for (int i = 0; i < 2; i++) {
+    std::cout << "Pid process: " << pid << std::endl;
+
+    for (int i = 0; i < 4; i++) {
         pthread_t tid;
         if (pthread_create(&tid, NULL, server, (void*) &data)) {
             std::cout << "Cannot create thread #" << i+1 << ". Aborted." << std::endl;
@@ -52,6 +56,10 @@ void *server(void *arg) {
     void *buffer = malloc(1024);  //valore un po' a caso, infatti è provvisorio
     int32_t bufflen = 0;
     bool success = false;
+    void *tempbuffer = malloc(1024); //un altro valore random, non farci caso
+
+    datarequested *req = NULL;
+    datareplied *rep = NULL;
 
     while (true) {
         std::cout << "Thread in attesa...\n";
@@ -60,36 +68,35 @@ void *server(void *arg) {
         socket.recv(&request);
 
         // interpreta richiesta
-        req_t message(request.data(), request.size());
-        std::cout << "Received key: " << message.key() << std::endl;
+        req = new datarequested( request.size(), request.data());
 
         // eseguie l'operazione richiesta
-        switch (message.op()) {
+        switch (req->op_code()) {
             case TYPE_OP_GET:
-                success = data->cache->get_item(message.key(), bufflen, &buffer);
+                std::cout << "richiesta get(" << req->key() << ")\n";
+                success = data->cache->get_item(req->key(), bufflen, &buffer);
+                rep = new datareplied(tempbuffer, buffer, (size_t) bufflen, TYPE_OP_GET, success);
                 break;
             case TYPE_OP_SET:
-                success = data->cache->store_item(message.key(), message.data(), message.datalen());
+                std::cout << "richiesta store(" << req->key() << ", " << req->datasize() << ")\n";
+                success = data->cache->store_item(req->key(), req->data(), req->datasize());
+                rep = new datareplied(tempbuffer, (void*) NULL, 0, TYPE_OP_SET, success);
                 break;
             case TYPE_OP_DELETE:
-                success = data->cache->delete_item(message.key());
-                break;
-            default:
-                success = false;
-                bufflen = 0;
+                std::cout << "richiesta delete(" << req->key() << ")\n";
+                success = data->cache->delete_item(req->key());
+                rep = new datareplied(tempbuffer, (void*) NULL, 0, TYPE_OP_DELETE, success);
                 break;
         }
 
-        rep_t *
-        to_send = message.op() == TYPE_OP_GET ?
-                  new rep_t(buffer, bufflen, success) :
-                  new rep_t(success, message.op());
+        std::cout << "byte reply: " << rep->size() << std::endl;
 
-        zmq::message_t reply(to_send->size());
-        memcpy((void *) reply.data(), to_send->binary(), to_send->size());
+        zmq::message_t reply(rep->size());
+        memcpy((void *) reply.data(), tempbuffer, rep->size());
         socket.send(reply);
 
-        delete(to_send);
+//        delete(req);
+//        delete(rep);
     }
 
     return NULL;
--- src/zmqclient.cpp
+++ src/zmqclient.cpp
@@ -1,22 +1,41 @@
+#include <iostream>
+#include <cstring>
 #include <zmq.hpp>
 #include "reqrep.hpp"
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " 'key to send' 'value to send'" << std::endl;
+        std::cerr << "Aborted." << std::endl;
+        return -1;
+    }
+
     zmq::context_t context(1);
     zmq::socket_t socket(context, ZMQ_REQ);
 
     socket.connect("tcp://localhost:5555");
 
-    req_t req("mastrota", 8, TYPE_OP_GET);
+    datarequested *req = NULL;
+    datareplied *rep = NULL;
+    void *buffer = malloc(1024); // un bel buffer random, che sei bellino!
 
-    zmq::message_t request(req.size());
-    memcpy((void*) request.data(), (void*) req.binary(), req.size());
-    socket.send(request);
+    for (int i = 0; i < 4; i++) {
+        req = new datarequested(buffer, argv[1], strlen(argv[1]), NULL, 0, TYPE_OP_GET);
+        zmq::message_t srequest(req->size());
 
-    zmq::message_t reply;
-    socket.recv(&reply);
-    rep_t rep(reply.data(), reply.size());
+        memcpy(srequest.data(), req->binary(), req->size());
+        socket.send(srequest);
 
+        zmq::message_t sreply;
+        socket.recv(&sreply);
+
+        rep = new datareplied(sreply.size(), sreply.data());
+
+        std::cout << "Esito operazione: " << rep->valret() << std::endl;
+
+        delete(req);
+        delete(rep);
+    }
 
     return 0;
 }
